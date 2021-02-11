d85114c4c28dc32e5781c448a153910748519f1e
jsingle345 <jsingle345@gmail.com>
2021-02-10 20:15:54 -0600

    Amazon header w/ search bar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import './App.css';
+
+function App() {
+  // BEM - Block, Element, Modifier
+  return (
+    <div className="app">
+      <Header />
+      {/* Header */}
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBChIfGkC1J7LXO9-cxnb7SzMukrmabZbQ",
+    authDomain: "clone-11c60.firebaseapp.com",
+    projectId: "clone-11c60",
+    storageBucket: "clone-11c60.appspot.com",
+    messagingSenderId: "556167498604",
+    appId: "1:556167498604:web:462bca44d421a863e38149",
+    measurementId: "G-G7S5G8X7L8"
+  };
