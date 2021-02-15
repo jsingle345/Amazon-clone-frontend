fb52ba7eb0340a8f11bffa4142ecd0354049746f
jsingle345 <jsingle345@gmail.com>
2021-02-14 20:15:53 -0600

    finished login structure

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header'
 import Home from './Home'
 import Checkout from './Checkout';
+import Login from './Login'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 
@@ -12,14 +13,17 @@ function App() {
   return (
     <Router> 
       <div className="app">
-        <Header />
+        
         <Switch> 
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout"> 
+            <Header />
             <Checkout />
           </Route>
           <Route path='/'>
-            
-            
+            <Header />
             <Home />
           </Route>
         </Switch>
